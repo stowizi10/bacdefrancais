11aab2389d944aa546879fea03772b735a9840f1
stowizi10 <158840287+stowizi10@users.noreply.github.com>
2025-07-27 21:01:54 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('anglais-cache').then(cache => {
+      return cache.addAll(['./', 'manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
